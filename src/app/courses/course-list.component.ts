69f2158d36a8c55db5b5348cca7a9ae67d0d5d15
Leonardo Dias <pauloleonardo_dp@outlook.com>
2020-05-26 15:16:58 -0300

    Substituindo selectors por rotas com Guards

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Course } from './course'
 import { CouseService } from './couse.service';
 @Component({
-  selector: 'app-course-list',
   templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
