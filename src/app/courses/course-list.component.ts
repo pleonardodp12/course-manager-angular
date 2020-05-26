62927afc41f653f24bba40e11dc8011600e14420
Leonardo Dias <pauloleonardo_dp@outlook.com>
2020-05-26 00:56:48 -0300

    Injeção de dependencia. Tratando eventos data binding, sistema de busca implementado

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,34 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { Course } from './courses'
+import { Course } from './course'
+import { CouseService } from './couse.service';
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
-  courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  _courses: Course[] = [];
+
+  _filterBy: string;
+
+  constructor(private courseService: CouseService){
+
+  }
+
   ngOnInit(): void{
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular: Forms',
-        imageUrl:'/assets/images/forms.png',
-        price: 99.99,
-        code: 'XPS-8976',
-        duration: 120,
-        rating: 5.4,
-        releaseDate: 'December,2,2019'
-      },
-      {
-        id: 2,
-        name: 'Angular: HTTP',
-        imageUrl: '/assets/images/http.png',
-        price: 45.99,
-        code: 'LKL-1096',
-        duration: 80,
-        rating: 4,
-        releaseDate: 'February,17,2020'
-      }
-    ]
+    this._courses = this.courseService.retrieveAll();
+    this.filteredCourses = this._courses;
+  }
+
+  set filter(value: string){
+    this._filterBy = value;
+
+    this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+  }
+
+  get filter(){
+    return this._filterBy;
   }
 
 }
